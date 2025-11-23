23ab43e948789bbdb67bd6841bbb43a76149d379
ecppexam <supplywork2025@gmail.com>
2025-11-23 12:17:03 +0700

    Update pretest2569_5_10.js

--- pretest2569_5_10.js
+++ pretest2569_5_10.js
@@ -2,10 +2,10 @@
 // CONFIGURATION
 // =====================================================
 const CONFIG = {
-    
+    // LIFF_ID: "2004722900-27YqP7Qk",
     LIFF_ID: "2008553229-nr0bN3Yl",
     SHEET_API_URL: 'https://script.google.com/macros/s/AKfycbwZvq4tvkdJPxlKABj0Dut1ljYCLSQhlL6nypoVQlPINbg3PF25-AjRxEpkp1N4xMCh/exec',
-    QUESTIONS_API_URL: 'https://script.google.com/macros/s/AKfycbyEGecABBOETRpo2R1Gtzg0YwgQJjwx8TKZtE-7ke_HfIDzmMCmtYJn2nj5c4Qagwi3KQ/exec',
+    QUESTIONS_API_URL: 'https://script.google.com/macros/s/AKfycbwjtjMGHdd7_EDWUvkCXlsJPoSipaj4w895OBqgPHgF34P987mU1ScA7KnNY3pO5AH7/exec',
     SAVE_SCORE_API_URL: 'https://script.google.com/macros/s/AKfycbyrm59LjB85rWLZD5GOQe7R4wjdCO_PkLv0bHtXB8k_hxIWVYF5gfpOEwJNDnTjcCK6fg/exec',
     NUM_QUESTIONS: 10,
     LOADING_DELAY: 4000,
@@ -205,9 +205,9 @@ async showResults() {
         }
         
         // Display wrong answers review
-        // this.displayWrongAnswers();
+        this.displayWrongAnswers();
         
-        // Utils.showElement('scoreCard');
+        Utils.showElement('scoreCard');
         
         // Save to localStorage
         if (state.userProfile) {
